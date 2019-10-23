58a75e703ea3d368269fc9f0265a5dcd35c69d67
suulcoder <ef.proyectomundial@gmail.com>
2019-10-23 14:38:57 -0600

    time

--- dht11.py
+++ dht11.py
@@ -1,12 +1,15 @@
+#------------------------------------------------------------------------------
 import sys
 import Adafruit_DHT
 import csv
 import random
+import datetime
+#------------------------------------------------------------------------------
 with open('data.csv', mode='w') as data_file:
 	data_file = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	data_file.writerow(['temperature'])
+	data_file.writerow(['date','temperature'])
 	while True:
 		humidity, temperature = Adafruit_DHT.read_retry(11,4)
 		temperature = temperature + random.uniform(-0.5, 0.5)
-		data_file.writerow([str(temperature)])
-		print('temperature: ' + str(temperature) +  ' °C | Humidity: ' + str(humidity))
+		data_file.writerow([str(datetime.datetime.now()),str(temperature)])
+		print('Date: ' + str(datetime.datetime.now()) + ' |temperature: ' + str(temperature))
